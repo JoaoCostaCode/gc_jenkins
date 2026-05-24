8b68e601ab795007da96634dfa64d6a0fbb37620
joao.costa <joao.costa.dev12@gmail.com>
2026-05-24 17:09:02 -0400

    correção do JUnit 4

--- src/test/java/com/conversor/AppTest.java
+++ src/test/java/com/conversor/AppTest.java
@@ -1,7 +1,6 @@
 package com.conversor;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class AppTest {
 
